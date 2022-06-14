8b70838af8b954d72c7fe5699f50f43b445ac703
LaZoRBear <lainesse.maxime@hotmail.com>
2022-06-14 12:08:48 -0400

    Initial Commit Basic template w/ Placeholder

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,9 +1,25 @@
 package control;
 
+import model.Categorie;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Categorie catJunior = new Categorie("Junior", 1.0, 1.0, 1.0, false);
+        Categorie catSenior = new Categorie("Senior", 1.0, 1.0, 1.0, false);
+        Categorie catSuper = new Categorie("Super", 1.0, 1.0, 1.0, true);
+
+        HashMap<String, Categorie> categories = new HashMap<>();
+
+        categories.put("Junior", catJunior);
+        categories.put("Senior", catSenior);
+        categories.put("Super", catSuper);
+
+        for (Categorie cat : categories.values()) {
+            System.out.println(cat);
+        }
 
     }
 }
--- src/model/Categorie.java
+++ src/model/Categorie.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Categorie {
+
+    String type; //Junior, Senior, Super
+    Double montantMaxBouffe;
+    Double montant2;
+    Double montant3;
+    Boolean droitAvion;
+
+    public Categorie(String type, Double montantMaxBouffe, Double montant2, Double montant3, Boolean droitAvion) {
+        this.type = type;
+        this.montantMaxBouffe = montantMaxBouffe;
+        this.montant2 = montant2;
+        this.montant3 = montant3;
+        this.droitAvion = droitAvion;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" +
+                "type='" + type + '\'' +
+                ", montantMaxBouffe=" + montantMaxBouffe +
+                ", montant2=" + montant2 +
+                ", montant3=" + montant3 +
+                ", droitAvion=" + droitAvion +
+                '}';
+    }
 }
